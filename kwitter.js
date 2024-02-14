bd9cbd8631837a8dc2aea11ccfc856f65e813cbb
AditiVedant <129192632+AditiVedant@users.noreply.github.com>
2024-02-14 15:03:46 +0400

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -1,5 +1,9 @@
-function addUser(){
+function addUser()
+{
+    
     user_name = document.getElementById("user_name").value;
-    localStorage.setItem("user_name" , user_name);
-    window.location = "kwitter_room.html";
+    
+    localStorage.setItem("user_name", user_name);
+    
+    window.location = "Kwitter_room.html";
 }
